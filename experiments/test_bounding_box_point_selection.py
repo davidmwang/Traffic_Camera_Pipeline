6468135c95211f61bd1c51286f5c2ada8f9a250a
davidmwang <dmwang@berkeley.edu>
2018-05-05 16:11:55 -0700

    Added method for selecting bounding box point by using centroid of transformed bounding box

--- experiments/test_bounding_box_point_selection.py
+++ experiments/test_bounding_box_point_selection.py
@@ -22,15 +22,15 @@ img = cv2.imread(os.path.join(VIDEO_NAME + "_images", '%s_%07d.jpg' % (VIDEO_NAM
 # cv2.imshow('img', img)
 # cv2.waitKey(100000)
 
-print(len(bboxes))
-print(bboxes[0])
+# print(len(bboxes))
+# print(bboxes[0])
 
-print(len(classes))
-print(classes[0])
+# print(len(classes))
+# print(classes[0])
 
-print(len(init_labels))
-print(init_labels[0])
-print(init_labels[1])
+# print(len(init_labels))
+# print(init_labels[0])
+# print(init_labels[1])
 
 cnfg = Config()
 hm = Homography(cnfg)
@@ -41,14 +41,93 @@ def choose_point_simple(bbox):
                                 use_offset=False)
     return result
 
+def compute_triangle_centroid(corners):
+    return np.mean(corners, axis=0)
+
+def line(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
+
+def intersection(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        x = Dx / D
+        y = Dy / D
+        return x,y
+    else:
+        return False
+
+def compute_centroid(corners):
+    # print(corners)
+    # print(corners[[0, 1, 2]])
+    centroids = np.zeros_like(corners)
+    # print(centroids.shape)
+    centroids[0] = compute_triangle_centroid(corners[[0, 1, 2]])
+    centroids[1] = compute_triangle_centroid(corners[[0, 1, 3]])
+    centroids[2] = compute_triangle_centroid(corners[[0, 2, 3]])
+    centroids[3] = compute_triangle_centroid(corners[[1, 2, 3]])
+    # print(compute_triangle_centroid(corners[[0, 1, 2]]))
+    # print(compute_triangle_centroid(corners[[0, 1, 3]]))
+
+    L1 = line(centroids[0], centroids[1])
+    L2 = line(centroids[2], centroids[3])
+
+    x, y = intersection(L1, L2)
+
+    return np.array([x, y])
+
+def choose_point_geometric(bbox):
+    x_min, y_min, x_max, y_max = bbox
+
+    corners = []
+    corners.append(hm.transform_point(x_max, (y_min + y_max) / 2.0, use_offset=False))
+    corners.append(hm.transform_point(x_min, y_max, use_offset=False))
+    corners.append(hm.transform_point(x_min, (y_min + y_max) / 2.0, use_offset=False))
+    corners.append(hm.transform_point(x_max, y_max, use_offset=False))
+    corners = np.array(corners)
+
+    # print(corners.shape)
+    # print(corners[0])
+    # print(corners[0].shape)
+
+    # axes = plt.gca()
+    # axes.set_xlim([-100,1100])
+    # axes.set_ylim([-100,1100])
+    # axes.invert_yaxis()
+
+    # axes.plot([400, 400], [0, 1000], color='k')
+    # axes.plot([500, 500], [0, 1000], color='k')
+    # axes.plot([600, 600], [0, 1000], color='k')
+
+    # axes.plot([0, 1000], [400, 400], color='k')
+    # axes.plot([0, 1000], [500, 500], color='k')
+    # axes.plot([0, 1000], [600, 600], color='k')
+
+    # axes.scatter(corners[:,0], corners[:,1])
+
+    centroid = compute_centroid(corners)
+    # print(centroid)
+
+    # axes.scatter(centroid[0], centroid[1])
+    # plt.show()
+
+    return centroid
+
+
+
+
 # test_bboxes = bboxes[0]
 # points = []
 # for i in range(len(test_bboxes)):
 #   print(test_bboxes[i])
-#   x_min, y_min, x_max, y_max = test_bboxes[i]
-#   result = hm.transform_point((x_min + x_max) / 2.0, y_max, use_offset=True)
-#   print(result)
-#   points.append(result)
+#   choose_point_geometric_opt(test_bboxes[i])
+  # result = hm.transform_point((x_min + x_max) / 2.0, y_max, use_offset=True)
+  # print(result)
+  # points.append(result)
 
 # points = np.array(points)
 # print(points.shape)
@@ -60,6 +139,8 @@ def choose_point_simple(bbox):
 # plt.imshow(img_warped)
 
 # plt.show()
+
+
 
 fig, axarr = plt.subplots(2, figsize=(5,10))
 
@@ -94,12 +175,12 @@ def animate(i):
     test_bboxes = bboxes[i]
     points = []
     for j in range(len(test_bboxes)):
-        points.append(choose_point_simple(test_bboxes[j]))
+        points.append(choose_point_geometric(test_bboxes[j]))
 
     points = np.array(points)
     scatter_plot.set_offsets(points)
 
     return img_plot, scatter_plot
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 100), interval=5, repeat=False)
+ani = animation.FuncAnimation(fig, animate, np.arange(400, 500), interval=5, repeat=False)
 plt.show()
